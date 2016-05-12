4e862a464078fb504699b031b736a2e1210023c1
Sebastian Callh <sebca553@mux2-14.edu.isy.liu.se>
2016-05-12 14:12:27 +0200

    added some output to assemble bash

--- assemble.sh
+++ assemble.sh
@@ -6,6 +6,7 @@ if [ "$file" ]; then
     python assembler.py "$file" 
     echo "Machine code successfully saved to file 'machine_code'"
     cat machine_code | xclip -selection clipboard
+    echo "Machine code succesfully copied to clipboard"
 else
     echo "No file specified"
 fi
